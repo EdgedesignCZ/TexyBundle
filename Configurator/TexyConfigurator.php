1f5b65e4b7cef0688ac0e030e722c8b84faa4d80
Marek Makovec <marovi@spadnul.net>
2013-08-30 16:39:51 +0200

    Better coding standards.

--- Configurator/TexyConfigurator.php
+++ Configurator/TexyConfigurator.php
@@ -32,21 +32,21 @@ class TexyConfigurator implements IConfigurator
      */
     public function configure(array $parameters)
     {
-        if(array_key_exists('class', $parameters)){
+        if (array_key_exists('class', $parameters)) {
             $texy = new $parameters['class'];
-            if(!$texy instanceof Texy){
+            if (!$texy instanceof Texy) {
                 throw new \InvalidArgumentException('Specified class ' . $parameters['class'] . ' is not instance of Texy nor it\'s descendant.');
             }
         } else {
             $texy = new $this->texyClassName;
         }
 
-        foreach($parameters as $type => $options){
-            if($type === 'allowed'){
+        foreach ($parameters as $type => $options) {
+            if ($type === 'allowed') {
                 $this->setAllowed($texy, $options);
-            } else if ($type === 'modules'){
+            } else if ($type === 'modules') {
                 $this->setModules($texy, $options);
-            } else if ($type === 'variables'){
+            } else if ($type === 'variables') {
                 $this->setVariables($texy, $options);
             }
         }
@@ -65,7 +65,7 @@ class TexyConfigurator implements IConfigurator
      */
     private function setAllowed(Texy $texy, $options)
     {
-        foreach($options as $option => $value){
+        foreach ($options as $option => $value) {
             $texy->allowed[$option] = $this->translateArgument($value);
         }
     }
@@ -82,8 +82,8 @@ class TexyConfigurator implements IConfigurator
      */
     private function setModules(Texy $texy, $options)
     {
-        foreach($options as $moduleName => $moduleOptions){
-            foreach($moduleOptions as $optionName => $optionParameters){
+        foreach ($options as $moduleName => $moduleOptions) {
+            foreach ($moduleOptions as $optionName => $optionParameters) {
                 $moduleVariableName = $moduleName.'Module';
                 $texy->$moduleVariableName->$optionName = $this->translateArgument($optionParameters);
             }
@@ -102,16 +102,16 @@ class TexyConfigurator implements IConfigurator
      */
     private function setVariables(Texy $texy, $options)
     {
-        foreach($options as $variableName => $value){
+        foreach ($options as $variableName => $value) {
             $texy->$variableName = $this->translateArgument($value);
         }
     }
 
     private function translateArgument($argument)
     {
-        if(is_array($argument)){
+        if (is_array($argument)) {
             $transformed_argument = array();
-            foreach($argument as $key => $value){
+            foreach ($argument as $key => $value) {
                 $transformed_argument[$key] = $this->translateValue($value);
             }
             $argument = $transformed_argument;
@@ -125,9 +125,9 @@ class TexyConfigurator implements IConfigurator
 
     private function translateValue($value)
     {
-        if($value === '*') {
+        if ($value === '*') {
             return Texy::ALL;
-        } else if ($value === '-'){
+        } else if ($value === '-') {
             return Texy::NONE;
         }
 
